265b24298c81ec447657f68791c85625136e0103
vngarg <sngargrsd@gmail.com>
2020-01-16 14:50:44 +0530

    Almost Done

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-function generator(){
-    var x=Math.floor((Math.random()*4)+1);
-    document.getElementById("divimg").innerHTML='<img src="inspire${x}.jpg">';
+var number_of_image=1;
+function display_image(){
+
+    if(number_of_image==1){
+        document.getElementById("image1").style.display="block";
+        ++number_of_image;
+        document.getElementById("image2").style.display="none";
+        document.getElementById("image3").style.display="none";
+        document.getElementById("image4").style.display="none";
+    }
+    else if(number_of_image==2){
+        document.getElementById("image2").style.display="block";
+        ++number_of_image
+        document.getElementById("image1").style.display="none";
+        document.getElementById("image3").style.display="none";
+        document.getElementById("image4").style.display="none";
+    }
+    else if(number_of_image==3){
+        document.getElementById("image3").style.display="block";
+        ++number_of_image
+        document.getElementById("image2").style.display="none";
+        document.getElementById("image1").style.display="none";
+        document.getElementById("image4").style.display="none";
+    }
+    else if(number_of_image==4){
+        document.getElementById("image4").style.display="block";
+        number_of_image=1
+        document.getElementById("image2").style.display="none";
+        document.getElementById("image3").style.display="none";
+        document.getElementById("image1").style.display="none";
+    }
 }
